438b232ecb9bf14b943be8aac1b27eb5a6cacba6
RitikGoderwal <117519145+RitikGoderwal@users.noreply.github.com>
2022-11-05 19:04:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,287 @@
+const container=document.getElementById('items-container');
+
+
+function him() {
+    const heading=document.getElementById('heading');
+    heading.innerHTML="Gifts for Men";
+
+  const imgone=document.getElementById('imageone');
+  imgone.src="wallet.webp";
+  imgone.style.width='250px';
+  imgone.style.width='250px';
+
+  const imgtwo=document.getElementById('imagetwo');
+  imgtwo.src="bottle.jpg";
+  imgtwo.style.width='250px';
+  imgtwo.style.width='250px';
+
+  const imgthree=document.getElementById('imagethree');
+  imgthree.src="watch.jpg";
+  imgthree.style.width='250px';
+  imgthree.style.width='250px';
+
+  const imgfour=document.getElementById('imagefour');
+  imgfour.src="case.jpg";
+  imgfour.style.width='250px';
+  imgfour.style.width='250px';
+
+  const imgfive=document.getElementById('imagefive');
+  imgfive.src="men5.jpg";
+  imgfive.style.width='250px';
+  imgfive.style.width='250px';
+
+  const imgsix=document.getElementById('imagesix');
+  imgsix.src="men6.jpg";
+  imgsix.style.width='250px';
+  imgsix.style.width='250px';
+
+  const imgseven=document.getElementById('imageseven');
+  imgseven.src="men7.jpg";
+  imgseven.style.width='250px';
+  imgseven.style.width='250px';
+
+  const imgeight=document.getElementById('imageeight');
+  imgeight.src="men8.jpg";
+  imgeight.style.width='250px';
+  imgeight.style.width='250px';
+
+
+  }
+
+  
+
+function her() {
+    const heading=document.getElementById('heading');
+    heading.innerHTML="Gifts for girls";
+
+  const imgone=document.getElementById('imageone');
+  imgone.src="her1.jpg";
+  imgone.style.width='250px';
+  imgone.style.width='250px';
+
+  const imgtwo=document.getElementById('imagetwo');
+  imgtwo.src="her2.jpg";
+  imgtwo.style.width='250px';
+  imgtwo.style.width='250px';
+
+  const imgthree=document.getElementById('imagethree');
+  imgthree.src="her3.jpg";
+  imgthree.style.width='250px';
+  imgthree.style.width='250px';
+
+  const imgfour=document.getElementById('imagefour');
+  imgfour.src="her4.jpg";
+  imgfour.style.width='250px';
+  imgfour.style.width='250px';
+
+  const imgfive=document.getElementById('imagefive');
+  imgfive.src="her5.jpg";
+  imgfive.style.width='250px';
+  imgfive.style.width='250px';
+
+  const imgsix=document.getElementById('imagesix');
+  imgsix.src="her6.jpg";
+  imgsix.style.width='250px';
+  imgsix.style.width='250px';
+  
+  const imgseven=document.getElementById('imageseven');
+  imgseven.src="her7.jpg";
+  imgseven.style.width='250px';
+  imgseven.style.width='250px';
+
+  const imgeight=document.getElementById('imageeight');
+  imgeight.src="her8.jpg";
+  imgeight.style.width='250px';
+  imgeight.style.width='250px';
+  
+
+  }
+
+  
+
+function kid() {
+    const heading=document.getElementById('heading');
+    heading.innerHTML="Gifts for kid";
+
+  const imgone=document.getElementById('imageone');
+  imgone.src="kid1.jpg";
+  imgone.style.width='250px';
+  imgone.style.width='250px';
+
+  const imgtwo=document.getElementById('imagetwo');
+  imgtwo.src="kid2.jpg";
+  imgtwo.style.width='250px';
+  imgtwo.style.width='250px';
+
+  const imgthree=document.getElementById('imagethree');
+  imgthree.src="kid3.jpg";
+  imgthree.style.width='250px';
+  imgthree.style.width='250px';
+
+  const imgfour=document.getElementById('imagefour');
+  imgfour.src="kid4.jpg";
+  imgfour.style.width='250px';
+  imgfour.style.width='250px';
+
+  const imgfive=document.getElementById('imagefive');
+  imgfive.src="kid5.jpg";
+  imgfive.style.width='250px';
+  imgfive.style.width='250px';
+
+  const imgsix=document.getElementById('imagesix');
+  imgsix.src="kid6.jpg";
+  imgsix.style.width='250px';
+  imgsix.style.width='250px';
+
+  const imgseven=document.getElementById('imageseven');
+  imgseven.src="kid7.jpg";
+  imgseven.style.width='250px';
+  imgseven.style.width='250px';
+
+  const imgeight=document.getElementById('imageeight');
+  imgeight.src="kid8.jpg";
+  imgeight.style.width='250px';
+  imgeight.style.width='250px';
+
+  }
+
+  
+
+function teen() {
+    const heading=document.getElementById('heading');
+    heading.innerHTML="gifts for teen";
+
+  const imgone=document.getElementById('imageone');
+  imgone.src="wallet.webp";
+  imgone.style.width='250px';
+  imgone.style.width='250px';
+
+  const imgtwo=document.getElementById('imagetwo');
+  imgtwo.src="bottle.jpg";
+  imgtwo.style.width='250px';
+  imgtwo.style.width='250px';
+
+  const imgthree=document.getElementById('imagethree');
+  imgthree.src="watch.jpg";
+  imgthree.style.width='250px';
+  imgthree.style.width='250px';
+
+  const imgfour=document.getElementById('imagefour');
+  imgfour.src="case.jpg";
+  imgfour.style.width='250px';
+  imgfour.style.width='250px';
+
+  const imgfive=document.getElementById('imagefive');
+  imgfive.src="kid5.jpg";
+  imgfive.style.width='250px';
+  imgfive.style.width='250px';
+
+  const imgsix=document.getElementById('imagesix');
+  imgsix.src="kid6.jpg";
+  imgsix.style.width='250px';
+  imgsix.style.width='250px';
+
+  const imgseven=document.getElementById('imageseven');
+  imgseven.src="kid7.jpg";
+  imgseven.style.width='250px';
+  imgseven.style.width='250px';
+
+  const imgeight=document.getElementById('imageeight');
+  imgeight.src="kid8.jpg";
+  imgeight.style.width='250px';
+  imgeight.style.width='250px';
+  }
+
+  
+
+function birthday() {
+    const heading=document.getElementById('heading');
+    heading.innerHTML="gifts for birhtday";
+
+  const imgone=document.getElementById('imageone');
+  imgone.src="wallet.webp";
+  imgone.style.width='250px';
+  imgone.style.width='250px';
+
+  const imgtwo=document.getElementById('imagetwo');
+  imgtwo.src="bottle.jpg";
+  imgtwo.style.width='250px';
+  imgtwo.style.width='250px';
+
+  const imgthree=document.getElementById('imagethree');
+  imgthree.src="watch.jpg";
+  imgthree.style.width='250px';
+  imgthree.style.width='250px';
+
+  const imgfour=document.getElementById('imagefour');
+  imgfour.src="case.jpg";
+  imgfour.style.width='250px';
+  imgfour.style.width='250px';
+
+  const imgfive=document.getElementById('imagefive');
+  imgfive.src="kid5.jpg";
+  imgfive.style.width='250px';
+  imgfive.style.width='250px';
+
+  const imgsix=document.getElementById('imagesix');
+  imgsix.src="him2.jpg";
+  imgsix.style.width='250px';
+  imgsix.style.width='250px';
+
+  const imgseven=document.getElementById('imageseven');
+  imgseven.src="him3.jpg";
+  imgseven.style.width='250px';
+  imgseven.style.width='250px';
+
+  const imgeight=document.getElementById('imageeight');
+  imgeight.src="him1.jpg";
+  imgeight.style.width='250px';
+  imgeight.style.width='250px';
+  }
+
+  
+
+function wedding() {
+    const heading=document.getElementById('heading');
+    heading.innerHTML="gifts for wedding";
+
+  const imgone=document.getElementById('imageone');
+  imgone.src="wd1.jpg";
+  imgone.style.width='250px';
+  imgone.style.width='250px';
+
+  const imgtwo=document.getElementById('imagetwo');
+  imgtwo.src="wd2.jpg";
+  imgtwo.style.width='250px';
+  imgtwo.style.width='250px';
+
+  const imgthree=document.getElementById('imagethree');
+  imgthree.src="wd3.jpg";
+  imgthree.style.width='250px';
+  imgthree.style.width='250px';
+
+  const imgfour=document.getElementById('imagefour');
+  imgfour.src="wd4.jpg";
+  imgfour.style.width='250px';
+  imgfour.style.width='250px';
+
+  const imgfive=document.getElementById('imagefive');
+  imgfive.src="her5.jpg";
+  imgfive.style.width='250px';
+  imgfive.style.width='250px';
+
+  const imgsix=document.getElementById('imagesix');
+  imgsix.src="her4.jpg";
+  imgsix.style.width='250px';
+  imgsix.style.width='250px';
+
+  const imgseven=document.getElementById('imageseven');
+  imgseven.src="her8.jpg";
+  imgseven.style.width='250px';
+  imgseven.style.width='250px';
+
+  const imgeight=document.getElementById('imageeight');
+  imgeight.src="her7.jpg";
+  imgeight.style.width='250px';
+  imgeight.style.width='250px';
+  }
